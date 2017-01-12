b3397193536f661b24fd004f29a8890ac6f1eb32
Your Name <you@example.com>
2017-01-12 22:03:56 +0000

    first commit

---
+++ push.sh
@@ -0,0 +1,11 @@
+#! /bin/sh 
+git init
+git add .
+ git commit -m "adding files"
+git remote add origin https://github.com/raouiyounes/12\-1
+#git pull origin  master
+
+ git push -u origin master
+
+
+
